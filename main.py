91f5c52506f5a561a1940ab8b7ac9f02dbd2ad4d
arif <OrangeBlu20R>
2021-06-07 03:05:42 +1000

    Done

--- main.py
+++ main.py
@@ -1,26 +1,30 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
+
 def celsius_to_fahrenheit(celsius):
     return (celsius * 9 / 5) + 32
+
 
 SAMPLE_SIZE = 100
 LEARNING_RATE = 0.01
 
-celsius_values = np.linspace(-50, 50, 100).reshape(SAMPLE_SIZE,1)
+celsius_values = np.linspace(-50, 50, 100).reshape(SAMPLE_SIZE, 1)
 fahrenheit_values = celsius_to_fahrenheit(celsius_values)
 
-weight1 = np.random.randn(1,3)
-bias1 = np.random.randn(1,3)
+weight1 = np.random.randn(1, 3)
+bias1 = np.random.randn(1, 3)
 
-weight2 = np.random.randn(3,1)
-bias2 = np.random.randn(1,1)
+weight2 = np.random.randn(3, 1)
+bias2 = np.random.randn(1, 1)
 
-def forward(x,y):
+
+def forward(x, y):
     output1 = np.dot(x, weight1) + bias1
     output2 = np.dot(output1, weight2) + bias2
     loss = np.square(output2 - y).sum() / x.shape[0]
     return output1, output2, loss
+
 
 def backward(x, y, output1, output2):
     grad_output2 = 2 * (output2 - y) / x.shape[0]
@@ -33,18 +37,30 @@ def backward(x, y, output1, output2):
 
     return grad_weight_1, grad_bias_1, grad_weight_2, grad_bias_2
 
-for t in range(3000):
-    output1, output2, loss = forward(celsius_values, fahrenheit_values)
-    grad_weight_1, grad_bias_1, grad_weight_2, grad_bias_2 = backward(
-        celsius_values, fahrenheit_values, output1, output2)
 
-    weight2 = weight2 - (LEARNING_RATE * grad_weight_2)
+def update_parameters(grad_weight_1, grad_bias_1, grad_weight_2, grad_bias_2):
+    global weight2, bias2, weight1, bias1
+    weight2= weight2 - (LEARNING_RATE * grad_weight_2)
     bias2 = bias2 - (LEARNING_RATE * grad_bias_2)
 
     weight1 = weight1 - (LEARNING_RATE * grad_weight_1)
     bias1 = bias1 - (LEARNING_RATE * grad_bias_1)
 
-_,predicted,_ = forward(celsius_values, fahrenheit_values)
-plt.plot(celsius_values, fahrenheit_values, 'b')
-plt.plot(celsius_values, predicted, 'r+')
+
+def train():
+    for t in range(2000):
+        output1, output2, loss = forward(celsius_values, fahrenheit_values)
+        grad_weight_1, grad_bias_1, grad_weight_2, grad_bias_2 = backward(
+            celsius_values, fahrenheit_values, output1, output2)
+        update_parameters(grad_weight_1, grad_bias_1, grad_weight_2, grad_bias_2)
+
+
+train()
+_, predicted, _ = forward(celsius_values, fahrenheit_values)
+fig, ax = plt.subplots()
+ax.plot(celsius_values, fahrenheit_values, '-b', label='Actual')
+ax.plot(celsius_values, predicted, '--r', label='Predicted')
+plt.xlabel("Celsius")
+plt.ylabel("Fahrenheit")
+ax.legend(loc='upper left', frameon=False)
 plt.show()
